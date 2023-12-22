f524614a9a64680eff3168a9f9b2b2d53fb14cc9
Kenji Saito <ken-yo@mbr.nifty.com>
2023-12-22 22:44:32 +0900

    Bumps node modules

--- function/index.ts
+++ function/index.ts
@@ -6,7 +6,9 @@ interface CloudFrontRequestEvent {
   request: CroudFrontRequest;
 }
 function handler(event: CloudFrontRequestEvent) {
+  // eslint-disable-next-line prefer-destructuring
   const request = event.request;
+  // eslint-disable-next-line prefer-destructuring
   const uri = request.uri;
 
   // Check whether the URI is missing a file name.
